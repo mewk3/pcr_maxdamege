7903184f96dc4885cac51c1e1c03f96a778465c0
mewk <93106989@qq.com>
2021-04-13 17:58:46 +0800

    :c

--- maofen.py
+++ maofen.py
@@ -34,9 +34,9 @@ score_rank = {'A1':1,'A2':1,'A3':1.1,'A4':1.1,'A5':1.2,
 #player_pool = res_char_player['char'].unique().tolist()  
 
 df_player = pd.read_excel('box.xlsx')
-player_pool = df_player['wkq'].dropna().astype(str).values.tolist()
+player_pool = df_player['temp'].dropna().astype(str).values.tolist()
 #df = pd.read_excel('rk.xlsx')
-df = pd.read_excel('homework.xlsx',sheet_name='Sheet3')
+df = pd.read_excel('homework.xlsx',sheet_name='C')
 test = pd.read_excel('./name_mapping.xlsx')
 
 def scan_team(str1):
